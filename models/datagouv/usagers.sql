6c24c4f5d5c6fbc7d52aabcb5490823a1e5e9041
papescayrewagon@gmail.com <papescayrewagon@gmail.com>
2023-08-29 10:35:31 +0000

    Dans la table usagers, recoder "secu" des années jusqu'à 2018 en "secu1".

--- models/datagouv/usagers.sql
+++ models/datagouv/usagers.sql
@@ -10,7 +10,16 @@ SELECT
   ,sexe
   ,an_nais
   ,trajet
-  ,secu AS secu1
+  ,CASE
+    WHEN secu IN (1,11) THEN 1
+    WHEN secu IN (2,21) THEN 2
+    WHEN secu IN (3,31) THEN 3
+    WHEN secu IN (4,41) THEN 4
+    WHEN secu IN (9,91) THEN 9
+    WHEN secu IN (0,12,22,32,42,92) THEN 0
+    WHEN secu IN (13,23,33,43,93) THEN 8
+    ELSE -1
+   END AS secu1
   ,NULL AS secu2
   ,NULL AS secu3
   ,etatp
