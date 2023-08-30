942f7630f70c0295e926b84d680c03e9b96b6d17
Sylvain <sylvainferrer.lewagon@gmail.com>
2023-08-30 13:33:56 +0000

    Create primary key (num_veh + Num_Acc)

--- models/staging/vehicules_stg.sql
+++ models/staging/vehicules_stg.sql
@@ -5,22 +5,27 @@ FROM {{ ref('vehicules') }}
 GROUP BY catv 
 ORDER BY catv_count DESC*/
 
-SELECT id_vehicule, num_veh, Num_Acc,
+WITH firstloop AS (
+SELECT
+CONCAT(num_veh,"_",Num_Acc) AS id_vehicule,
+id_vehicule AS id_vehicule_OLD, Num_Acc, num_veh,
 CASE
-        WHEN motor = 1 THEN "Hydrocarbures"
-        WHEN motor = 2 THEN "Hybride électrique"
-        WHEN motor = 3 THEN "Electrique"
-        WHEN motor = 4 THEN "Hydrogène"
-        WHEN motor = 5 THEN "Humaine"
-        ELSE "Autres"
-    END AS motor,
+WHEN motor = 1 THEN "Hydrocarbures"
+WHEN motor = 2 THEN "Hybride électrique"
+WHEN motor = 3 THEN "Electrique"
+WHEN motor = 4 THEN "Hydrogène"
+WHEN motor = 5 THEN "Humaine"
+ELSE "Autres"
+END AS motor,
 catv,
-    CASE
-        WHEN catv = 7 THEN "Véhicules légers"
-        WHEN catv = 33 OR catv = 34 OR catv = 43 THEN "2 Roues > 125cm2"
-        WHEN catv = 2 OR catv = 30 OR catv = 31 OR catv = 32 OR catv = 42 OR catv = 41 THEN "2 Roues < 125cm2"
-        WHEN catv = 10 THEN "Véhicules utilitaires"
-        WHEN catv = 80 OR catv = 50 OR catv = 60 OR catv = 1 THEN "Vélos et trotinettes"
-        ELSE "Autres"
-    END AS catv_sgt
+CASE
+WHEN catv = 7 THEN "Véhicules légers"
+WHEN catv = 33 OR catv = 34 OR catv = 43 THEN "2 Roues > 125cm2"
+WHEN catv = 2 OR catv = 30 OR catv = 31 OR catv = 32 OR catv = 42 OR catv = 41 THEN "2 Roues < 125cm2"
+WHEN catv = 10 THEN "Véhicules utilitaires"
+WHEN catv = 80 OR catv = 50 OR catv = 60 OR catv = 1 THEN "Vélos et trotinettes"
+ELSE "Autres"
+END AS catv_sgt
 FROM {{ ref('vehicules') }}
+)
+SELECT * FROM firstloop
